c3e4e431fe31d2cb1cb4db8faf6707fdb70145c0
adarshEr2020 <adarshprajapati31@gmail.com>
2022-03-13 03:00:36 +0530

    useReducer hook

---
+++ src/componants/hooks/useReducer-hook/HookuseReducer.jsx
@@ -0,0 +1,35 @@
+import React, { useReducer } from "react";
+
+const initialstate = 0;
+
+// useReducer hook manage the state we are using when we are working on big  project and that time we
+// can use useReducer managing the state
+
+const reducer = (state, action) => {
+  console.log(state, action);
+  if (action.type == "INCREMENT") {
+    return state + 1;
+  }
+  if (action.type == "DECREMENT") {
+    return state - 1;
+  }
+  return state;
+};
+
+const HookuseReducer = () => {
+  //   connst[(count, setCount)] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialstate);
+  return (
+    <div>
+      <h1>useReducer hook</h1>
+      <p>{state}</p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>increment</button>
+      <br />
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>decrement</button>
+      <br />
+    </div>
+  );
+};
+
+export default HookuseReducer;
